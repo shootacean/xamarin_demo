a13718cc8f6630460179e68c60aea02e12fbf909
shootacean <shootacean@gmail.com>
2017-07-05 08:02:16 +0900

    add httpclient

--- xamarin_demo/xamarin_demo/MainPage.xaml.cs
+++ xamarin_demo/xamarin_demo/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using System.Net.Http;
+
 namespace xamarin_demo
 {
     public partial class MainPage : ContentPage
@@ -12,6 +14,29 @@ namespace xamarin_demo
         public MainPage()
         {
             InitializeComponent();
+
+            this.BtnPost.Clicked += BtnPost_Clicked;
+        }
+
+        private async void BtnPost_Clicked(object sender, EventArgs e)
+        {
+            var res = await Api();
+            var msg = "";
+            foreach (var s in res)
+                msg += s;
+
+            this.LblResponse.Text = msg;
+        }
+
+        private async Task<String> Api()
+        {
+            ; const string url = @"http://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060";
+            var hc = new HttpClient();
+            var res = new HttpResponseMessage();
+            res = await hc.GetAsync(url);
+            var result = await res.Content.ReadAsStringAsync();
+            //return await hc.GetStringAsync(url);
+            return result;
         }
     }
 }
